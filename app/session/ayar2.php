d376db990b58f04d97bfb077a930f9a37fed98bd
YakultIndonesia <dije423@gmail.com>
2025-03-10 10:24:13 +0700

    Delete app directory

--- app/session/ayar2.php
+++
@@ -1,7 +0,0 @@
-<?php
-include ("../app/config.php");
-$sorgu=$spexcon->query("SELECT * FROM admin ");               
-$cikti =$sorgu->fetch_assoc();               
-$user=$cikti['user'];
-$pass=$cikti['pass'];
-?>
--- app/session/session.php
+++
@@ -1,9 +0,0 @@
-<?php 
-include("ayar.php");
-session_start();
-if (!isset($_SESSION["login"])) {
-echo "<center><b><h3>Bu Sayfayı Görüntüleme<br>Yetkiniz Yoktur!</h3></b></center>";
-header("Location:../../admin/index.php");
- }else{
-}
-?>
